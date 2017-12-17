edda3919b8db7973b308068aa59c792c5c2c2baa
yejunyu <liuxing_yue@126.com>
2017-12-17 21:35:37 +0800

    字符串转换

---
+++ src/numberAndString/MyStringBuffer/Test.java
@@ -0,0 +1,13 @@
+package numberAndString.MyStringBuffer;
+
+/**
+ * @author yejunyu
+ * @date 2017/12/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        MyStringBuffer a = new MyStringBuffer("abc");
+        a.reverse();
+        System.out.println();
+    }
+}
