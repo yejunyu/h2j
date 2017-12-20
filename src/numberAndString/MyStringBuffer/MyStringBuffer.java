6cb65427754dd433e057d65ef03af410a0d377b5
yy <liuxing_yue@126.com>
2017-12-20 15:04:58 +0800

    toString

--- src/numberAndString/MyStringBuffer/MyStringBuffer.java
+++ src/numberAndString/MyStringBuffer/MyStringBuffer.java
@@ -2,31 +2,34 @@ package numberAndString.MyStringBuffer;
 
 import java.util.Arrays;
 
-public class MyStringBuffer implements IStringBuffer {
+public class MyStringBuffer implements IStringBuffer{
     int capacity = 16;
     int length = 0;
     char[] value;
 
-    public MyStringBuffer() {
+    public MyStringBuffer(){
         value = new char[capacity];
     }
 
     //有参构造方法
-    public MyStringBuffer(String str) {
-        if (null != str) {
-            value = str.toCharArray();
+    public MyStringBuffer(String str){
+        if(null!=str){
+            value =str.toCharArray();
         }
         length = value.length;
-        if (capacity < value.length) {
+        if(capacity<value.length) {
             capacity = value.length * 2;
         }
         char[] newValue = new char[capacity];
-        System.arraycopy(value, 0, newValue, 0, length);
+        System.arraycopy(value,0,newValue,0,length);
         value = newValue;
     }
 
     @Override
     public String toString() {
+        char[] newValue = new char[length];
+        System.arraycopy(value, 0, newValue, 0, length);
+        value = newValue;
         return Arrays.toString(value);
     }
 
@@ -47,26 +50,26 @@ public class MyStringBuffer implements IStringBuffer {
 
     @Override
     public void insert(int pos, String b) {
-        if (pos < 0 || pos > length) {
+        if (pos<0 || pos>length){
             return;
         }
-        if (b == null) {
+        if(b==null){
             return;
         }
         char[] insertB = b.toCharArray();
         int newLength = b.length();
-        if (length + newLength > capacity) {
+        if (length+newLength>capacity){
             capacity = capacity * 2;
         }
         char[] newValue = new char[capacity];
         // 1. 元素后移
         // value pos 之后的元素全都后移 String b 长度的位置
-        System.arraycopy(value, pos, newValue, pos + newLength, length - pos);
+        System.arraycopy(value,pos,newValue,pos + newLength,length-pos);
         // 2. 插入元素
         // pos 位置插入 String b 的长度
-        System.arraycopy(insertB, 0, newValue, pos, newLength);
+        System.arraycopy(insertB,0,newValue,pos,newLength);
         // pos 之前的不变
-        System.arraycopy(value, 0, newValue, 0, pos);
+        System.arraycopy(value,0,newValue,0,pos);
         value = newValue;
         length = length + newLength;
     }
@@ -83,10 +86,10 @@ public class MyStringBuffer implements IStringBuffer {
 
     @Override
     public void reverse() {
-        for (int i = 0; i < length / 2; i++) {
+        for (int i = 0; i < length/2; i++) {
             char temp = value[i];
-            value[i] = value[length - i - 1];
-            value[length - i - 1] = temp;
+            value[i] = value[length-i-1];
+            value[length-i-1] = temp;
         }
     }
 
