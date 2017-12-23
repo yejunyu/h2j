b059c939efc401e731d70bf8187d1d676e6729fd
yejunyu <liuxing_yue@126.com>
2017-12-23 21:25:55 +0800

    ArrayList 遍历

---
+++ src/MyGeneric/Test.java
@@ -0,0 +1,67 @@
+package MyGeneric;
+
+import MyArrayListTest.Hero;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author yejunyu
+ * @date 2017/12/23.
+ */
+public class Test {
+    static void test(){
+        /**
+         * 首先初始化一个Hero集合，里面放100个Hero对象，名称分别是从
+         hero 0
+         hero 1
+         hero 2
+         ...
+         hero 99.
+
+         通过遍历的手段，删除掉名字编号是7的倍数和带7的对象
+         */
+        List<Hero> Heros = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Heros.add(new Hero("hero"+i));
+        }
+        for (int i = 1; i < Heros.size(); i++) {
+            String num = Heros.get(i).getName().substring(4);
+            if (num.contains("7") || Integer.valueOf(num) % 7 == 0){
+                // 一定要减减,因为删除元素的时候索引变了,比如27,28,就会出问题
+                Heros.remove(i--);
+            }
+        }
+        System.out.println(Heros);
+    }
+    public static void main(String[] args) {
+        /**
+         * List 遍历
+         */
+        // 迭代器
+        List<Hero> Heros = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Heros.add(new Hero("hero" + i));
+        }
+        // Iterator 第一种写法
+        Iterator<Hero> Hero = Heros.iterator();
+        while (Hero.hasNext()) {
+            Hero a = Hero.next();
+            System.out.println(a);
+        }
+
+        // Iterator 第一种写法
+        for (Iterator<Hero> Hero1 = Heros.iterator(); Hero1.hasNext(); ) {
+            Hero b = Hero1.next();
+            System.out.println(b);
+        }
+
+        // foreach
+        for (MyArrayListTest.Hero hero : Heros) {
+            System.out.println(hero);
+        }
+
+        test();
+    }
+}
--- src/numberAndString/MyStringBuffer/IStringBuffer.java
+++ src/numberAndString/MyStringBuffer/IStringBuffer.java
@@ -8,8 +8,8 @@ public interface IStringBuffer {
     void append(char c) throws IndexIsNegativeException, IndexIsOutofRangeException;  //追加字符
     void insert(int pos,char b) throws IndexIsNegativeException, IndexIsOutofRangeException; //指定位置插入字符
     void insert(int pos,String b) throws IndexIsNegativeException, IndexIsOutofRangeException; //指定位置插入字符串
-    void delete(int start); //从开始位置删除剩下的
-    void delete(int start,int end); //从开始位置删除结束位置-1
+    void delete(int start) throws IndexIsNegativeException, IndexIsOutofRangeException; //从开始位置删除剩下的
+    void delete(int start,int end) throws IndexIsNegativeException, IndexIsOutofRangeException; //从开始位置删除结束位置-1
     void reverse(); //反转
     int length(); //返回长度
 }
