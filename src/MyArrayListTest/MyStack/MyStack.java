385e0da881a02f8feaeab804b20d39d796160e63
yejunyu <liuxing_yue@126.com>
2017-12-26 22:36:03 +0800

    MyStack

--- src/MyArrayListTest/MyStack/MyStack.java
+++ src/MyArrayListTest/MyStack/MyStack.java
@@ -2,6 +2,7 @@ package MyArrayListTest.MyStack;
 
 import MyArrayListTest.Hero;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -10,23 +11,46 @@ import java.util.Stack;
  */
 public class MyStack implements Stackk{
 
+    LinkedList<Hero> values;
 
-    @Override
-    public void push(Hero h) {
-
+    public MyStack() {
+        values = new LinkedList<>();
     }
 
     @Override
-    public Hero pull() {
-        return null;
+    public String toString() {
+        StringBuilder StringBuilder = new StringBuilder();
+        for (Hero value : values) {
+            StringBuilder.append(value);
+            StringBuilder.append("\t");
+        }
+        return StringBuilder.toString();
+    }
+
+    @Override
+    public void push(Hero h) {
+        values.offer(h);
+    }
+
+    @Override
+    public Hero pop() {
+        return values.removeLast();
     }
 
     @Override
     public Hero peek() {
-        return null;
+        return values.getLast();
     }
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        MyStack MyStack = new MyStack();
+        MyStack.push(new Hero("hero1"));
+        MyStack.push(new Hero("hero2"));
+        MyStack.push(new Hero("hero3"));
+        MyStack.push(new Hero("hero4"));
+        System.out.println(MyStack.peek());
+        System.out.println(MyStack.pop());
+        System.out.println(MyStack);
     }
 }
--- src/MyArrayListTest/MyStack/Stackk.java
+++ src/MyArrayListTest/MyStack/Stackk.java
@@ -7,10 +7,21 @@ import MyArrayListTest.Hero;
  * @date 2017/12/25.
  */
 public interface Stackk {
-    //把英雄推入到最后位置
+    /**
+     * 把英雄推入到最后位置
+     * @param h
+     */
     public void push(Hero h);
-    //把最后一个英雄取出来
-    public Hero pull();
-    //查看最后一个英雄
+
+    /**
+     * 把最后一个英雄取出来
+     * @return
+     */
+    public Hero pop();
+
+    /**
+     * 查看最后一个英雄
+     * @return
+     */
     public Hero peek();
 }
