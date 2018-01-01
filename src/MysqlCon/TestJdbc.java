cc319f6afa0ce98b26710b6a8c9ff855b60425cd
yejunyu <liuxing_yue@126.com>
2018-01-01 08:16:17 +0800

    jdbcTest

--- src/MysqlCon/TestJdbc.java
+++ src/MysqlCon/TestJdbc.java
@@ -1,9 +1,6 @@
 package MysqlCon;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * @author yejunyu
@@ -21,8 +18,26 @@ public class TestJdbc {
             System.out.println(c);
             s = c.createStatement();
             System.out.println(s);
+            // 原生 sql 语句
             String sql = "insert into hero values(null,'ti',500.05,34)";
             s.execute(sql);
+            // 查询
+            sql = "SELECT * FROM hero";
+            ResultSet rs = s.executeQuery(sql);
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString(2);
+                double HP = rs.getDouble("HP");
+                double damage = rs.getDouble("damage");
+                System.out.printf("%d\t%s\t%f\t%f%n", id, name, HP, damage);
+            }
+            // 带占位符的原生 sql 语句
+            sql = "INSERT INTO hero VALUES (NULL,?,?,?)";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1,"gg");
+            ps.setDouble(2,500);
+            ps.setDouble(3,35);
+            ps.execute();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
