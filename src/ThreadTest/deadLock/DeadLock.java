7d22113a567dfdd6901f364530e2ac387b61a1a0
yejunyu <liuxing_yue@126.com>
2018-02-25 17:49:57 +0800

    死锁

---
+++ src/ThreadTest/deadLock/DeadLock.java
@@ -0,0 +1,54 @@
+package ThreadTest.deadLock;
+
+import ThreadTest.Hero;
+
+/**
+ * @author yejunyu
+ * @date 2018/2/25.
+ */
+public class DeadLock {
+    public static void main(String[] args) {
+        Hero a = new Hero();
+        a.name = "a";
+        Hero b = new Hero();
+        b.name = "b";
+
+        Thread t1 = new Thread(){
+            @Override
+            public void run() {
+                synchronized (a){
+                    System.out.println("占有 a");
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+                    System.out.println("等待 b");
+                    synchronized (b){
+                        System.out.println("让 b 做 something");
+                    }
+                }
+            }
+        };
+        t1.start();
+        Thread t2 = new Thread(){
+            @Override
+            public void run() {
+                synchronized (b){
+                    System.out.println("占有 b");
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+                    System.out.println("等待 a");
+                    synchronized (a){
+                        System.out.println("让 a 做 something");
+                    }
+                }
+            }
+        };
+        t2.start();
+    }
+
+}
